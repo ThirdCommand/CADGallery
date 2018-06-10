e1aa565cc8cf07bf78f2ff4d02b977d9bbaef599
SecondCommand <nathaniel.cunha@gmail.com>
2018-06-10 16:13:36 -0700

    setup heroku

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,6 +3,7 @@ class Api::SessionsController < ApplicationController
   def create 
     @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
     
+    @user
     if @user
       login(@user)
       render "/api/users/show"
