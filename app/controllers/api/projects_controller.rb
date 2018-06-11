6daf0cafb9a8a7cab4ceefc8e3b7fa0546b26fc7
SecondCommand <nathaniel.cunha@gmail.com>
2018-06-11 10:09:56 -0700

    comment out pictures/projects controllers

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,32 +1,32 @@
 class API::ProjectsController < ApplicationController
 
-  def index
-    @projects = Project.all
-  end
+#   def index
+#     @projects = Project.all
+#   end
 
-  def show 
-    @project = Project.find(params[:id])
-  end 
+#   def show 
+#     @project = Project.find(params[:id])
+#   end 
 
-  def create 
-    @project = Project.new(project_params)
-    if @project.save
-      # go to projects index when completed
-      render "/api/projects"
-    else 
-      render json: @projects.errors.full_messages, status 422
-    end
-  end
+#   def create 
+#     @project = Project.new(project_params)
+#     if @project.save
+#       # go to projects index when completed
+#       render "/api/projects"
+#     else 
+#       render json: @projects.errors.full_messages, status: 422
+#     end
+#   end
 
-  def destroy 
-    @project = Project.find(params[:d])
-    @project.delete 
-    render "api/projects"
-  end
+#   def destroy 
+#     @project = Project.find(params[:d])
+#     @project.delete 
+#     render "api/projects"
+#   end
 
-private
+# private
   
-  def project_params 
-    params.require(:project).permit(:title, :description, :user_id)
-  end
+#   def project_params 
+#     params.require(:project).permit(:title, :description, :user_id)
+#   end
 end
